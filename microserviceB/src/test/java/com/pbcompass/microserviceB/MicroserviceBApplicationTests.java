4a519043576558f72c21136ea9c37e2bf12cec47
João Ponchiroli <joaopedroponqueroli@gmail.com>
2024-11-25 16:32:48 -0300

    build: Adding project base to repository

---
+++ microserviceB/src/test/java/com/pbcompass/microserviceB/MicroserviceBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pbcompass.microserviceB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroserviceBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

6ceb6da27bdd751b8880a29fa580d51bdb54d0c9
Matheus Reato <matheusreato13@gmail.com>
2024-11-26 14:19:30 -0300

    feature: post mapper creation

---
+++ microserviceB/src/main/java/com/pbcompass/microserviceB/mapper/PostMapper.java
@@ -0,0 +1,12 @@
+package com.pbcompass.microserviceB.mapper;
+
+import com.pbcompass.microserviceB.dto.PostDTO;
+import com.pbcompass.microserviceB.entity.Post;
+import org.mapstruct.Mapper;
+@Mapper
+public interface PostMapper {
+
+    PostDTO toDTO(Post post);
+
+    Post toPost(PostDTO dto);
+}

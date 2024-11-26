b690bfb5112903bbfbd5fe9443f6bcd774af4c23
DallaProRacing <103423@aluno.uricer.edu.br>
2024-11-26 16:23:25 -0300

    feature: delete post requisition

--- microserviceB/src/main/java/com/pbcompass/microserviceB/controller/PostController.java
+++ microserviceB/src/main/java/com/pbcompass/microserviceB/controller/PostController.java
@@ -1,13 +1,11 @@
 package com.pbcompass.microserviceB.controller;
 
 import com.pbcompass.microserviceB.entity.Post;
+import com.pbcompass.microserviceB.repository.PostRepository;
 import com.pbcompass.microserviceB.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/posts")
@@ -16,10 +14,19 @@ public class PostController {
     @Autowired
     PostService postService;
 
+
+
     @GetMapping(value = "/{id}")
     public ResponseEntity<Post> findById(@PathVariable String id) {
         Post obj = postService.findById(id);
         return ResponseEntity.ok().body(obj);
+
+    }
+
+    @RequestMapping(value="/{id}", method= RequestMethod.DELETE)
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        postService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
--- microserviceB/src/main/java/com/pbcompass/microserviceB/service/PostService.java
+++ microserviceB/src/main/java/com/pbcompass/microserviceB/service/PostService.java
@@ -4,7 +4,11 @@ import com.pbcompass.microserviceB.entity.Post;
 import com.pbcompass.microserviceB.repository.PostRepository;
 import com.pbcompass.microserviceB.service.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Optional;
 
@@ -17,5 +21,12 @@ public class PostService {
     public Post findById(String id) {
         Optional<Post> post = postRepository.findById(id);
         return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+
     }
+
+    public void delete(String id) {
+        postRepository.findById(id).orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+        postRepository.deleteById(id);
+    }
+
 }

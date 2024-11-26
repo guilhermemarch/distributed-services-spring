909e26f7b57b30c8597379f967977f4a881eaa2b
DallaProRacing <103423@aluno.uricer.edu.br>
2024-11-26 14:15:46 -0300

    feature: post dto creation

--- microserviceB/src/main/java/com/pbcompass/microserviceB/dto/PostDTO.java
+++ microserviceB/src/main/java/com/pbcompass/microserviceB/dto/PostDTO.java
@@ -1,4 +1,34 @@
 package com.pbcompass.microserviceB.dto;
 
-public class PostDTO {
+import lombok.*;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+
+public class PostDTO implements Serializable {
+
+    private String documentId;
+    private Long postId;
+    private Long userId;
+    private String title;
+    private String body;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PostDTO postDTO = (PostDTO) o;
+        return Objects.equals(documentId, postDTO.documentId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(documentId);
+    }
 }

b9457a52f56d23c8ba4aea6f9334eed8e6513a1e
Matheus Reato <matheusreato13@gmail.com>
2024-11-28 13:10:52 -0300

    feature: creating test to post requisition with invalid data

--- microserviceB/src/main/java/com/pbcompass/microserviceB/dto/PostDTO.java
+++ microserviceB/src/main/java/com/pbcompass/microserviceB/dto/PostDTO.java
@@ -26,4 +26,10 @@ public class PostDTO implements Serializable {
     @NotBlank
     private String body;
 
+    public PostDTO(String title, String body) {
+        this.title = title;
+        this.body = body;
+    }
+
+
 }
